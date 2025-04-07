e8d8df574a30d0da13acdc1a9ca908f931ee1820
tenshialk <mariacicera0981@gmail.com>
2025-04-07 19:22:47 +0000

    15

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/usuario_create')
 def usuario_create():
     return render_template('usuario_create.html')
