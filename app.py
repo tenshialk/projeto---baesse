ade7a240c7bd384dabd23ec3949ebc0e978f62cb
tenshialk <mariacicera0981@gmail.com>
2025-04-07 19:21:50 +0000

    10

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/usuario_create')
 def usuario_create():
-   return render_template('usuario_create.html')
+    return render_template('usuario_create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
